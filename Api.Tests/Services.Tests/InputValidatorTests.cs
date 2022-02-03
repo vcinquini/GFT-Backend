ba7a4ed2c1ceca3c657d09f71ce05a6c75bcc865
Valmir Cinquini <vcinquini@gmail.com>
2022-02-03 15:02:10 -0300

    Input Validator unit tests

---
+++ Api.Tests/Services.Tests/InputValidatorTests.cs
@@ -0,0 +1,51 @@
+﻿using Application.Interfaces;
+using Application.Validators;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Tests.Services.Tests
+{
+	public class InputValidatorTests
+	{
+		private readonly IInputValidator _inputValidator;
+
+		public InputValidatorTests()
+		{
+			_inputValidator = new InputValidator();
+		}
+
+		[Theory]
+		[InlineData("morning", "1", "2", "3")]
+		public void CheckInputArguments_When_DayTime_IsMIssing_Should_ReturnError(params string[] inputs)
+		{
+			// Act
+			Action action = () => _inputValidator.CheckInputArguments(inputs);
+
+			action
+				.Should()
+				.NotThrow<ArgumentException>();
+		}
+
+		[Theory]
+		[InlineData("1", "2", "3")]
+		[InlineData("moning")]
+		[InlineData("night")]
+		public void CheckInputArguments_When_Parameter_IsMIssing_Should_ReturnError(params string[] inputs)
+		{
+			// Act
+			Action action = () => _inputValidator.CheckInputArguments(inputs);
+
+			action
+				.Should()
+				.Throw<ArgumentException>();
+		}
+
+
+	}
+}
